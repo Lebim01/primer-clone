889e6fa1d28b4360cd4f358c18eb6b444e02640a
Victor Alvarez <victor.alvarez@mtrade.mx>
2022-10-02 10:20:59 -0500

    checkout fix

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react"
 import CheckoutBuildContextProvider from "@src/pages/checkoutBuild/checkout.build.context"
 import CheckoutBuildSidemenuContextProvider, { useCheckoutBuildSidemenuContext } from "@src/pages/checkoutBuild/CheckoutSideMenu/checkout.build.sidemenu.context"
 import CheckoutSideMenu from "@src/pages/checkoutBuild/CheckoutSideMenu/CheckoutSideMenu"
@@ -6,20 +7,24 @@ import { AnimatePresence, motion } from "framer-motion"
 const Checkout = () => {
   const { paymentMethods } = useCheckoutBuildSidemenuContext()
 
+  const activeMethods = useMemo(() => paymentMethods.filter(r => r.active), [paymentMethods])
+
   return (
     <div className="flex h-full w-full">
       <CheckoutSideMenu />
       <div className="flex w-full items-center justify-center bg-neutral-200">
         <motion.div className="checkout flex w-[350px] flex-col gap-2 rounded-lg border bg-white p-8 shadow">
           <AnimatePresence>
-            {paymentMethods.filter(r => r.active).map((method) => 
+            {activeMethods.map((method) => 
               <motion.div key={method.name} className="h-12 w-full" initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
                 {method.button}
               </motion.div>
             )}
-            <div className="relative my-2 border-t" key="label">
-              <span className="absolute left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white px-3 text-sm">Or pay with card</span>
-            </div>
+            {activeMethods.length > 0 &&
+              <div className="relative my-2 border-t" key="label">
+                <span className="absolute left-1/2 -translate-y-1/2 -translate-x-1/2 bg-white px-3 text-sm">Or pay with card</span>
+              </div>
+            }
             <div className="mt-2 flex flex-col gap-3" key="form">
               <div className="flex flex-col gap-2">
                 <span className="text-base font-medium">Card number</span>
