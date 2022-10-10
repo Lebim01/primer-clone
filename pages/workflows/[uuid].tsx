a15eaab62dd49e09d533591503e39f0c9058924b
Victor Alvarez <victor.alvarez@mtrade.mx>
2022-10-10 17:14:57 -0500

    test cabeceras

--- pages/workflows/[uuid].tsx
+++ pages/workflows/[uuid].tsx
@@ -6,6 +6,7 @@ import WorkflowBuildContextProvider, { useWorkflowBuildContext } from "@src/page
 import WorkflowBuildSidemenuContextProvider from "@src/pages/workflowBuild/WorkflowAppsSideMenu/workflow.build.sidemenu.context";
 import { StepType, TourProvider, useTour } from '@reactour/tour'
 import { wait } from "@src/utils/tour";
+import Head from "next/head";
 
 const TOUR_NAME = "tour-edit-flow"
 const steps: StepType[] = [
@@ -129,6 +130,26 @@ const Container = () => (
       localStorage.setItem(TOUR_NAME, "1"); 
     }}
   >
+    <Head>
+      {/*<!-- Primary Meta Tags -->*/}
+      <title>Meta Tags — Preview, Edit and Generate</title>
+      <meta name="title" content="Meta Tags — Preview, Edit and Generate"/>
+      <meta name="description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"/>
+
+      {/*<!-- Open Graph / Facebook -->*/}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://metatags.io/" />
+      <meta property="og:title" content="Meta Tags — Preview, Edit and Generate" />
+      <meta property="og:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
+      <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+      {/*<!-- Twitter -->*/}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://metatags.io/" />
+      <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate" />
+      <meta property="twitter:description" content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!" />
+      <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+    </Head>
     <ReactFlowProvider>
       <WorkflowBuildContextProvider>
         <WorkflowBuildSidemenuContextProvider>
